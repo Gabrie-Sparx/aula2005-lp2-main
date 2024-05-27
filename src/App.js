2c8699edc1473fad910b371a4684bed8341d187f
gabriel <xsparx10@gmail.com>
2024-05-26 23:09:21 -0300

    aula2005-lp2-main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { cloneElement } from 'react';
+import InputCustomizado from '../src/Inputcustomizado.tsx';
+import '../src/App.css';
 
-function App() {
+const App = () => {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [phone, setPhone] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div id="contato">
+      <h2>Entre em contato</h2>
+      <p></p>
+    <form onSubmit={handleSubmit}>
+    
+      <div >
+      <label>
+        <InputCustomizado
+          type="text"
+          defaultValue="Nome"
+          placeholder=""
+        />
+      </label>
+      </div>
+      <div class="campos">
+      <label>
+        <InputCustomizado
+          type="email"
+          defaultValue="E-mail"
+          placeholder=""
+        />
+      </label>
+      </div>
+      
+      <div>
+      <label>
+        <InputCustomizado
+          type="number"
+          defaultValue="Telefone"
+          placeholder=""
+        />
+      </label>
+      </div>
+      <div></div>
+      <div>
+        <p></p>
+        <p id="botao">
+        <button type="submit">Fatec: Enviar mensagem</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    </form>
     </div>
   );
-}
+};
 
 export default App;
